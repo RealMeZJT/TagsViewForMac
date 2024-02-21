d2442da15defe85dca687800eef5d59511ce44ad
ZhouJiatao <709855120@qq.com>
2024-02-21 11:20:25 +0800

    Initial Commit

---
+++ TagsViewForMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TagsViewForMac
+//
+//  Created by jiatao zhou on 2024/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TagsViewForMac/TagsViewForMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagsViewForMacApp.swift
+//  TagsViewForMac
+//
+//  Created by jiatao zhou on 2024/2/21.
+//
+
+import SwiftUI
+
+@main
+struct TagsViewForMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
